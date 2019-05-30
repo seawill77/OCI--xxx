06f495349169d189172008d9a54acaaf610f39b6
Xiaosong Lu <31264128+seawill77@users.noreply.github.com>
2019-05-30 13:54:57 -0700

    Update _127_WordLadder.java

--- Leetcode-1-300-master/1-600/_127_WordLadder.java
+++ Leetcode-1-300-master/1-600/_127_WordLadder.java
@@ -39,6 +39,44 @@ public class _127_WordLadder {
      * @param wordList
      * @return
      */
+    
+    
+    public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
+    wordList.add(endWord);
+    Queue<String> queue = new LinkedList<String>();
+    queue.add(beginWord);
+    int level = 0;
+    while(!queue.isEmpty()){
+        int size = queue.size();
+        for(int i = 0; i < size; i++){
+            String cur = queue.remove();
+            if(cur.equals(endWord)){ return level + 1;}
+            for(int j = 0; j < cur.length(); j++){
+                char[] word = cur.toCharArray();
+                for(char ch = 'a'; ch < 'z'; ch++){
+                    word[j] = ch;
+                    String check = new String(word);
+                    if(!check.equals(cur) && wordList.contains(check)){
+                        queue.add(check);
+                        wordList.remove(check);
+                    }
+                }
+            }
+        }
+        level++;
+    }
+    return 0;
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     public int ladderLength1(String beginWord, String endWord, List<String> wordList) {
         HashSet<String> set = new HashSet<>(wordList);
         if (set.contains(beginWord)) {
