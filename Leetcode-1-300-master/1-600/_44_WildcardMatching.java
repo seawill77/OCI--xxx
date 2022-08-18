43d6bd433075de9d05a954174a52daad34d111c4
Xiaosong Lu <31264128+seawill77@users.noreply.github.com>
2022-08-18 16:39:17 -0700

    Update _44_WildcardMatching.java

--- Leetcode-1-300-master/1-600/_44_WildcardMatching.java
+++ Leetcode-1-300-master/1-600/_44_WildcardMatching.java
@@ -73,28 +73,30 @@ public class _44_WildcardMatching {
 
 //一个 *  only
 
-
-int sp = 0;
-        int pp = 0;
-        int star = -1;
-        
-        
-        while (sp < s.length()) {
-            
-            if (pp < p.length() && s.charAt(sp) == p.charAt(pp)) {
-                sp++;
-                pp++;
-            } else if (pp < p.length() && p.charAt(pp) == '*') {
-                star = pp;
-                pp++;
-            } else if (star != -1) {
-                pp = star + 1;
-                sp++;
-            } else {
-                return false;
-            }
-
-        }
-        
-        
-        return  pp == p.length();
+public boolean isMatch_2d_method(String s, String p) {
+	int m=s.length(), n=p.length();
+	boolean[][] dp = new boolean[m+1][n+1];
+	dp[0][0] = true;
+	for (int i=1; i<=m; i++) {
+		dp[i][0] = false;
+	}
+	
+	for(int j=1; j<=n; j++) {
+		if(p.charAt(j-1)=='*'){
+			dp[0][j] = true;
+		} else {
+			break;
+		}
+	}
+	
+	for(int i=1; i<=m; i++) {
+		for(int j=1; j<=n; j++) {
+			if (p.charAt(j-1)!='*') {
+				dp[i][j] = dp[i-1][j-1] && (s.charAt(i-1)==p.charAt(j-1) || p.charAt(j-1)=='?');
+			} else {
+				dp[i][j] = dp[i-1][j] || dp[i][j-1];
+			}
+		}
+	}
+	return dp[m][n];
+}
